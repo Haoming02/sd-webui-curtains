25658af4573d412b0a4019f20b2848f8910a8eaa
Haoming <hmstudy02@gmail.com>
2024-02-23 10:58:56 +0800

    no longer covers buttons

--- javascript/curtains.js
+++ javascript/curtains.js
@@ -1,7 +1,7 @@
 ﻿onUiLoaded(async () => {
     ['txt', 'img'].forEach((tab) => {
 
-        const result = document.getElementById(`${tab}2img_results`);
+        const result = document.getElementById(`${tab}2img_gallery`);
         const curtain = document.createElement('div');
         curtain.classList.add(`${tab}2img_curtains`);
         result.appendChild(curtain);
