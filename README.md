56dbaef506af3a7b22ff480859820e628abb1ee4
Haoming <hmstudy02@gmail.com>
2024-03-07 18:02:42 +0800

    implement #1

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ This is an Extension for the [Automatic1111 Webui](https://github.com/AUTOMATIC1
 ## Wut
 When you don't want the passerby to see what you are generating, just add a curtain over the results that folds away when hovered~
 
+## How to Use
+By default, the curtain is folded away on the **right**. Simply click on it to let it expand. Click on it again to fold it once more.
+
 <hr>
 
 <sup>~~Did I spend too much effort making the curtains look good?~~</sup>
--- javascript/curtains.js
+++ javascript/curtains.js
@@ -4,6 +4,18 @@
         const result = document.getElementById(`${tab}2img_gallery`);
         const curtain = document.createElement('div');
         curtain.classList.add(`${tab}2img_curtains`);
+        curtain.classList.add('curtain-off');
+
+        curtain.addEventListener('click', () => {
+            if (curtain.classList.contains('curtain-on')) {
+                curtain.classList.remove('curtain-on');
+                curtain.classList.add('curtain-off');
+            } else {
+                curtain.classList.remove('curtain-off');
+                curtain.classList.add('curtain-on');
+            }
+        });
+
         result.appendChild(curtain);
 
     });
