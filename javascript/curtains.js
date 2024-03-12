c7546e3626579603efcbb5240d3996ea157554d8
Haoming <hmstudy02@gmail.com>
2024-03-12 15:13:57 +0800

    add a curtain to img2img input too

--- javascript/curtains.js
+++ javascript/curtains.js
@@ -3,7 +3,7 @@
 
         const result = document.getElementById(`${tab}2img_gallery`);
         const curtain = document.createElement('div');
-        curtain.classList.add(`${tab}2img_curtains`);
+        curtain.classList.add('curtains');
         curtain.classList.add('curtain-off');
 
         curtain.addEventListener('click', () => {
@@ -19,4 +19,22 @@
         result.appendChild(curtain);
 
     });
+
+    const tabs = document.getElementById('mode_img2img');
+    const curtain = document.createElement('div');
+    curtain.classList.add('curtains');
+    curtain.classList.add('curtain-off');
+
+    curtain.addEventListener('click', () => {
+        if (curtain.classList.contains('curtain-on')) {
+            curtain.classList.remove('curtain-on');
+            curtain.classList.add('curtain-off');
+        } else {
+            curtain.classList.remove('curtain-off');
+            curtain.classList.add('curtain-on');
+        }
+    });
+
+    tabs.appendChild(curtain);
+
 });
