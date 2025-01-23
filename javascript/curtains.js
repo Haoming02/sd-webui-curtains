e89a71f8f13ebdc650025242e9be5a7381090618
Haoming <hmstudy02@gmail.com>
2025-01-23 09:53:00 +0800

    lint

--- javascript/curtains.js
+++ javascript/curtains.js
@@ -1,40 +1,31 @@
-﻿onUiLoaded(async () => {
-    ['txt', 'img'].forEach((tab) => {
+﻿(function () {
+    onUiLoaded(() => {
+        for (const tab of ['txt', 'img']) {
+            const result = document.getElementById(`${tab}2img_gallery`);
 
-        const result = document.getElementById(`${tab}2img_gallery`);
+            const curtain = document.createElement('div');
+            curtain.classList.add('curtains');
+            curtain.classList.add('curtain-off');
+
+            curtain.addEventListener('click', () => {
+                curtain.classList.toggle('curtain-on');
+                curtain.classList.toggle('curtain-off');
+            });
+
+            result.appendChild(curtain);
+        }
+
+        const i2i_input = document.getElementById('mode_img2img');
+
         const curtain = document.createElement('div');
         curtain.classList.add('curtains');
         curtain.classList.add('curtain-off');
 
         curtain.addEventListener('click', () => {
-            if (curtain.classList.contains('curtain-on')) {
-                curtain.classList.remove('curtain-on');
-                curtain.classList.add('curtain-off');
-            } else {
-                curtain.classList.remove('curtain-off');
-                curtain.classList.add('curtain-on');
-            }
+            curtain.classList.toggle('curtain-on');
+            curtain.classList.toggle('curtain-off');
         });
 
-        result.appendChild(curtain);
-
+        i2i_input.appendChild(curtain);
     });
-
-    const tabs = document.getElementById('mode_img2img');
-    const curtain = document.createElement('div');
-    curtain.classList.add('curtains');
-    curtain.classList.add('curtain-off');
-
-    curtain.addEventListener('click', () => {
-        if (curtain.classList.contains('curtain-on')) {
-            curtain.classList.remove('curtain-on');
-            curtain.classList.add('curtain-off');
-        } else {
-            curtain.classList.remove('curtain-off');
-            curtain.classList.add('curtain-on');
-        }
-    });
-
-    tabs.appendChild(curtain);
-
-});
+})();
